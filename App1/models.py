a1fd2bf042cdfcd8d44e677ff6abf9eecc4eba86
louisboucard <louis44.boucard@gmail.com>
2024-12-18 14:39:05 +0100

    tt

--- App1/models.py
+++ App1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField(default=0)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
